dd89bcf1dbcc6bf94f133004e684599589422f50
Daminer5 <20599157+Daminer5@users.noreply.github.com>
2026-03-27 03:57:43 +0000

    Update manifest.lua

--- manifest.lua
+++ manifest.lua
@@ -1,41 +1,41 @@
 return {
   -- Common files installed on all nodes
   common = {
-    "lib/updater.lua",
-    "lib/updater.cfg",
+    "/lib/updater.lua",
+    "/lib/updater.cfg",
   },
 
   -- Node-specific files
   nodes = {
     hmi = {
-      "lib/nodes/hmi.lua",
-      "lib/nodes/config/hmi.lua"
+      "/lib/nodes/hmi.lua",
+      "/lib/nodes/config/hmi.lua"
     },
     orchestrator = {
-      "lib/nodes/orchestrator.lua",
-      "lib/nodes/config/orchestrator.lua"
+      "/lib/nodes/orchestrator.lua",
+      "/lib/nodes/config/orchestrator.lua"
     },
     br_reactor = {
-      "lib/hardware/br_reactor.lua",
-      "lib/nodes/config/br_reactor.lua"
+      "/lib/hardware/br_reactor.lua",
+      "/lib/nodes/config/br_reactor.lua"
     },
     ae_storage = {
-      "lib/hardware/ae2_monitor.lua",
-      "lib/nodes/config/ae_storage.lua"
+      "/lib/hardware/ae2_monitor.lua",
+      "/lib/nodes/config/ae_storage.lua"
     },
     exporter = {
-      "lib/nodes/exporter.lua",
-      "lib/nodes/config/exporter.lua"
+      "/lib/nodes/exporter.lua",
+      "/lib/nodes/config/exporter.lua"
     },
     database = {
-      "lib/database.lua",
-      "lib/nodes/config/database.lua"
+      "/lib/database.lua",
+      "/lib/nodes/config/database.lua"
     },
     worker = {
-      "lib/nodes/config/worker.lua"
+      "/lib/nodes/config/worker.lua"
     },
     generic = {
-      "lib/nodes/config/generic.lua"
+      "/lib/nodes/config/generic.lua"
     }
   }
 }
