3a3024ea0e15057e9d5f066a2983baa3319e166d
Daminer5 <20599157+Daminer5@users.noreply.github.com>
2026-03-26 21:50:24 -0400

    modified:   manifest.lua 	modified:   version.txt

--- manifest.lua
+++ manifest.lua
@@ -60,16 +60,24 @@ return {
   -- Node-specific files
   nodes = {
     hmi = {
-      "lib/nodes/orchestrator.lua",
+      "lib/nodes/hmi.lua",
       "lib/nodes/config/hmi.lua"
     },
-    reactor = {
-      "lib/hardware/br_reactor.lua",
-      "lib/nodes/config/reactor.lua"
+    orchestrator = {
+      "lib/nodes/orchestrator.lua",
+      "lib/nodes/config/orchestrator.lua"
     },
-    storage = {
+    br_reactor = {
+      "lib/hardware/br_reactor.lua",
+      "lib/nodes/config/br_reactor.lua"
+    },
+    ae_storage = {
       "lib/hardware/ae2_monitor.lua",
-      "lib/nodes/config/storage.lua"
+      "lib/nodes/config/ae_storage.lua"
+    },
+    exporter = {
+      "lib/nodes/exporter.lua",
+      "lib/nodes/config/exporter.lua"
     },
     database = {
       "lib/database.lua",
